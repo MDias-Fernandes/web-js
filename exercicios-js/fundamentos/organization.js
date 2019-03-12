b68eeffa0c9de208f292fff01063fa996e92d0c2
MDias-Fernandes <diasmatheus621@gmail.com>
2019-03-12 00:16:13 -0300

    New push

---
+++ exercicios-js/fundamentos/organization.js
@@ -0,0 +1,10 @@
+console.log(
+    "Sentenca de codigo, que pode ocupar uma linha ou mais") // Sentenca de codigo
+
+{
+    //bloco de codigo, que também pode ser vazio
+    {
+        console.log('Aspas simples')
+        console.log("Aspas duplas")
+    }
+}
